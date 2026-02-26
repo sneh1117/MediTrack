7f58759406915eabdc550a4648b19c3350da9ac8
sneha <sneha.naik1117@gmail.com>
2026-02-26 16:34:47 +0530

    Test user 1

--- accounts/admin.py
+++ accounts/admin.py
@@ -19,3 +19,18 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Additional Info', {'fields': ('role', 'phone', 'date_of_birth')}),
     )
+
+
+from django.db import migrations
+
+def create_superuser():
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='sneh111',
+            email='sneh111@meditrack.com',
+            password='AutoChembur123*'
+        )
+
+create_superuser()
