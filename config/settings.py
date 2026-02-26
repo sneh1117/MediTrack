2df1f204e3451683744c6d7a077f9bc87f02775f
sneha <sneha.naik1117@gmail.com>
2026-02-26 17:18:01 +0530

    fix database config to use DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -87,14 +87,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 from decouple import config
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+import dj_database_url
+import os
 # Development: Using SQLite (switch to PostgreSQL for production)
 DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': BASE_DIR / 'db.sqlite3',
-       }
-   }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 
 # Password validation
