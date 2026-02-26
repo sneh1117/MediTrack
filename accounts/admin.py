30d2fa7f8ef3e990b966543b0761aa9884617393
sneha <sneha.naik1117@gmail.com>
2026-02-26 16:43:29 +0530

    Test user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -26,7 +26,7 @@ from django.db import migrations
 def create_superuser():
     from django.contrib.auth import get_user_model
     User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
+    if not User.objects.filter(username='sneh111').exists():
         User.objects.create_superuser(
             username='sneh111',
             email='sneh111@meditrack.com',
