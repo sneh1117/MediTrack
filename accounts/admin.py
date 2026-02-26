978678d6472f8a4d43093af6a04c8f2a73bed7f5
sneha <sneha.naik1117@gmail.com>
2026-02-26 17:05:12 +0530

    MEDITRACK Admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -21,16 +21,4 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
-from django.db import migrations
 
-def create_superuser():
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username='sneh111').exists():
-        User.objects.create_superuser(
-            username='sneh111',
-            email='sneh111@meditrack.com',
-            password='AutoChembur123*'
-        )
-
-create_superuser()
