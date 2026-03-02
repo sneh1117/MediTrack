b1627d15194d35361876f61aaafc3a94abc9b0d4
sneha <sneha.naik1117@gmail.com>
2026-03-02 12:57:43 +0530

    Changed to vercel name Final

--- config/settings.py
+++ config/settings.py
@@ -177,7 +177,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://meditrack.vercel.app",
+    "https://meditrack7.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
