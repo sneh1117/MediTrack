3ae04312cb7fb8872e6746c81e7e4218d180e924
sneha <sneha.naik1117@gmail.com>
2026-03-06 11:42:25 +0530

    CSRF Issues solved

--- config/settings.py
+++ config/settings.py
@@ -174,8 +174,10 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     'https://meditrack.up.railway.app',
+    'https://meditrack7.vercel.app',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    
 ]
 
 AUTH_USER_MODEL='accounts.User'
