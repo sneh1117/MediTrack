4ce364b2aae11b82f6d8204db2e0c335328358e0
sneha <sneha.naik1117@gmail.com>
2026-03-06 16:00:43 +0530

    Changing the port again to 465

--- config/settings.py
+++ config/settings.py
@@ -236,8 +236,9 @@ CELERY_BEAT_SCHEDULE = {
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
