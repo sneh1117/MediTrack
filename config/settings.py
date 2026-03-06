5ebbb0c8497bf366286047a617afa41f8a176fe0
sneha <sneha.naik1117@gmail.com>
2026-03-06 18:08:47 +0530

    Updated settings

--- config/settings.py
+++ config/settings.py
@@ -236,15 +236,6 @@ CELERY_BEAT_SCHEDULE = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", cast=bool)
-
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 
 if not DEBUG:
